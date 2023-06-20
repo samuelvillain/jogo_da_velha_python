6fa6a8e331158d983503eb61f6e0877d347c3fc4
Samuel Villain <samuelsvillain@gmail.com>
2023-06-20 15:46:01 -0300

    vitoria pronta

--- app.py
+++ app.py
@@ -1,21 +1,3 @@
-def grade_igual(grade):
-
-    for linha in grade:
-        if len(set(linha)) != 1:
-            return False
-
-    for coluna in zip(*grade):
-        if len(set(coluna)) != 1:
-            return False
-
-    diagonal_principal = [grade[i][i] for i in range(len(grade))]
-    if len(set(diagonal_principal)) != 1:
-        return False
-
-    diagonal_secundaria = [grade[i][len(grade)-i-1] for i in range(len(grade))]
-    if len(set(diagonal_secundaria)) != 1:
-        return False
-
 def matriz():
     linha1 = [None, None, None]
     linha2 = [None, None, None]
@@ -23,51 +5,71 @@ def matriz():
 
     grade = [linha1, linha2, linha3]
 
-    if grade_igual(grade) == grade:
-        print("Parabéns você ganhou!!!")
-
     return grade
-
 
 def imprimir_grade(grade):
     for elemento in grade:
         print(elemento)
 
 
+def verificar_vitoria():
+    ganha_linha = None
+
+    for linha in grade:
+        if all(elemento == linha[0] for elemento in linha) and linha[0] is not None:
+            ganha_linha = linha[0]
+            break
+
+    for coluna in range(3):
+        if all(grade[linha][coluna] == grade[0][coluna] for linha in range(3)) and grade[0][coluna] is not None:
+            ganha_linha = grade[0][coluna]
+            break
+
+    if grade[0][0] == grade[1][1] == grade[2][2] and grade[0][0] is not None:
+        ganha_linha = grade[0][0]
+    elif grade[0][2] == grade[1][1] == grade[2][0] and grade[0][2] is not None:
+        ganha_linha = grade[0][2]
+
+    return ganha_linha
+
 grade = matriz()
 imprimir_grade(grade)
-grade_igual(grade)
 
 while True:
     while True:
         while True:
-            linha = int(input("Digite o número da linha que deseja? "))
+            linha = int(input("Digite o número da linha que deseja: "))
             if linha > 2:
-                print("Valor %d inválido, digite o número de 0 até 2" % linha)
+                print("Valor %d inválido. Digite um número de 0 a 2." % linha)
                 continue
             break
 
         while True:
-            coluna = int(input("Digite o número da coluna que deseja? "))
+            coluna = int(input("Digite o número da coluna que deseja: "))
             if coluna > 2:
-                print("Valor %d inválido, digite o número de 0 até 2" % coluna)
+                print("Valor %d inválido. Digite um número de 0 a 2." % coluna)
                 continue
             break
 
         if grade[linha][coluna] is not None:
-            print("Posicao ja tem valor. Informe outro valor")
+            print("Posição já tem valor. Informe outro valor.")
             continue
         break
 
-        while True:
-            valor = input("X ou O? ")
+    while True:
+        valor = input("X ou O? ")
         if valor != "x" and valor != "o":
-            print("Valor %s inválido, digite X ou O" % valor)
+            print("Valor %s inválido. Digite X ou O." % valor)
             continue
         break
-
-
 
     grade[linha][coluna] = valor
     imprimir_grade(grade)
-    grade_igual(grade)
+
+    ganhador = verificar_vitoria()
+    if ganhador:
+        print("Parabéns você venceu!")
+        break
+
+
+
